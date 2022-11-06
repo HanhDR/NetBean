32f0e6d63e939c5bd76b5afcc4b70553d498ddc5
HanhDR <114923359+HanhDR@users.noreply.github.com>
2022-11-06 21:35:40 +0700

    Add files via upload

--- Rmi_nhom4/src/dao/KetNoi.java
+++ Rmi_nhom4/src/dao/KetNoi.java
@@ -7,20 +7,22 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  *
  * @author hanht
  */
 public class KetNoi {
-    public Connection cn;
+    public static Connection cn;
     public void KetNoi() {
         try {
 //            Ket noi vao csdl: QLTaiKhoan
             //b1: xac dinh he quan tri csdl
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             System.out.println("DA KET NOI HQTCSDL");
-            String url="jdbc:sqlserver://LAPTOP-V5E394L3\\SQLEXPRESS:1433;databaseName=QLTaiKhoan;user=sa;password=123123";
+            String url="jdbc:sqlserver://LAPTOP-TNBT312N\\SQLEXPRESS:1433;databaseName=QLTK;user=sa;password=1234";
             cn = DriverManager.getConnection(url);
             System.out.println("Da Ket NOI");
         } catch (Exception e) {
@@ -29,7 +31,19 @@ public class KetNoi {
     }
     
     public static void main(String[] args) {
-        KetNoi kn = new KetNoi();
+        try {
+               KetNoi kn = new KetNoi();
         kn.KetNoi();
+        ChiTietTaiKhoandao ctdao=new ChiTietTaiKhoandao();
+        taikhoandao tkdao=new taikhoandao();
+        tkdao.KtDn("01234","12344");
+        String st="2021-10-10";
+        SimpleDateFormat dd=new SimpleDateFormat("yyyy-mm-dd");
+        Date ngay=dd.parse(st);
+        ctdao.Them(ngay,12300000 ,"01234","alo");
+        
+        } catch (Exception e) {
+        }
+        
     }
 }
